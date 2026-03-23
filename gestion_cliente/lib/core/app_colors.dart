65f7e064440cafbe8f79199087f4bc2c4ca795e8
amo0037 <amo0037@alu.medac.es>
2026-03-23 12:03:09 +0100

    Create app_colors.dart

---
+++ gestion_cliente/lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const primaryColor = Color.fromARGB(255, 0, 98, 179)   
+  static const secondaryColor = Color.fromARGB(255, 3, 71, 218);
+  static const backgroundColor = Color.fromARGB(255, 28, 0, 150);  
+  static const errorColor = Color.fromARGB(171, 255, 30, 0);
+}
+
