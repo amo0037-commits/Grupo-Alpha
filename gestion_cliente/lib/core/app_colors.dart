5f38c1c5895d71d5b6782ec13e3a5a724669af7a
amo0037 <amo0037@alu.medac.es>
2026-03-23 15:19:30 +0100

    Mejora colores/ temas
    Creación de la clase app_themes

--- gestion_cliente/lib/core/app_colors.dart
+++ gestion_cliente/lib/core/app_colors.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/animation.dart';
+
 //Colores para aplicar a las distintas ventanas de la App, se pueden agregar más o modificar los existentes
+
 class AppColors {
-  static const primaryColor = Color.fromARGB(255, 21, 101, 192);   
-  static const secondaryColor = Color.fromARGB(255, 66, 165, 245);
-  static const backgroundColor = Color.fromARGB(255, 115, 183, 231);  
+  static const primaryColor = Color(0xFF1565C0);   
+  static const secondaryColor = Color(0xFF42A5F5);
+  static const backgroundColor = Color(0xFF73B7E7);  
 
-  static const primaryColor2 = Color.fromARGB(255, 0, 151, 167);
-  static const secondaryColor2 = Color.fromARGB(255, 77, 208, 225);
-  static const backgroundColor2 = Color.fromARGB(255, 176, 240, 248);
+  static const primaryColor2 = Color.fromARGB(255, 85, 217, 231);
+  static const secondaryColor2 = Color(0xFF4DD0E1);
+  static const backgroundColor2 = Color.fromARGB(255, 172, 235, 243);
 
-  static const primaryColor3 = Color.fromARGB(255, 0, 179, 45);
-  static const secondaryColor3 = Color.fromARGB(255, 10, 218, 3);
-  static const backgroundColor3 = Color.fromARGB(255, 0, 150, 112);
+  static const primaryColor3 = Color(0xFF00B32D);
+  static const secondaryColor3 = Color(0xFF0ADA03);
+  static const backgroundColor3 = Color(0xFF009670);
 
-  static const primaryColor4 = Color.fromARGB(255, 142, 36, 170);
-  static const secondaryColor4 = Color.fromARGB(255, 186, 104, 200);
-  static const backgroundColor4 = Color.fromARGB(255, 242, 194, 250);
+  static const primaryColor4 = Color(0xFF8E24AA);
+  static const secondaryColor4 = Color(0xFFBA68C8);
+  static const backgroundColor4 = Color(0xFFF2C2FA);
 
-  static const primaryColor5 = Color.fromARGB(255, 198, 40, 40);
-  static const secondaryColor5 = Color.fromARGB(255, 239, 83, 80);
-  static const backgroundColor5 = Color.fromARGB(255, 243, 186, 194);
+  static const primaryColor5 = Color(0xFFC62828);
+  static const secondaryColor5 = Color(0xFFEF5350);
+  static const backgroundColor5 = Color(0xFFF3BAC2);
 
   
 }
---
+++ gestion_cliente/lib/core/app_themes.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:gestion_cliente/core/app_colors.dart';
+
+// Temas aplicables a las distintas ventanas de la app y a sus respectivos elementos.
+class AppThemes {
+
+  //Tema de la pantalla de inicio.
+
+    static final InicioTheme = ThemeData(
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.backgroundColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.backgroundColor,
+      foregroundColor: Colors.white,
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primaryColor,
+      primary: AppColors.primaryColor,
+      secondary: AppColors.secondaryColor,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        foregroundColor: Colors.white,
+      ),
+    ),
+  );
+
+   //Tema Azul claro.
+
+    static final AzulClaroTheme = ThemeData(
+    primaryColor: AppColors.primaryColor2,
+    scaffoldBackgroundColor: AppColors.backgroundColor2,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.backgroundColor2,
+      foregroundColor: Colors.white,
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primaryColor2,
+      primary: AppColors.primaryColor2,
+      secondary: AppColors.secondaryColor2,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        foregroundColor: Colors.white,
+      ),
+    ),
+  );
+
+   //Tema Verde.
+
+    static final VerdeTheme = ThemeData(
+    primaryColor: AppColors.primaryColor3,
+    scaffoldBackgroundColor: AppColors.backgroundColor3,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.backgroundColor3,
+      foregroundColor: Colors.white,
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primaryColor3,
+      primary: AppColors.primaryColor3,
+      secondary: AppColors.secondaryColor3,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor3,
+        foregroundColor: Colors.white,
+      ),
+    ),
+  );
+
+   //Tema Morado/Rosa.
+
+    static final MoradoTheme = ThemeData(
+    primaryColor: AppColors.primaryColor4,
+    scaffoldBackgroundColor: AppColors.backgroundColor4,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.backgroundColor4,
+      foregroundColor: Colors.white,
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primaryColor4,
+      primary: AppColors.primaryColor4,
+      secondary: AppColors.secondaryColor4,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        foregroundColor: Colors.white,
+      ),
+    ),
+  );
+
+   //Tema Rojo.
+   
+    static final RojoTheme = ThemeData(
+    primaryColor: AppColors.primaryColor5,
+    scaffoldBackgroundColor: AppColors.backgroundColor5,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.backgroundColor5,
+      foregroundColor: Colors.white,
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primaryColor5,
+      primary: AppColors.primaryColor5,
+      secondary: AppColors.secondaryColor5,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor5,
+        foregroundColor: Colors.white,
+      ),
+    ),
+  );
+}
