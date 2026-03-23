283c9c71330ed27ca84c0046d2c499f29504b2d6
amo0037 <amo0037@alu.medac.es>
2026-03-23 12:27:30 +0100

    Arreglo Colores
    Arreglo clase app_Colors

--- gestion_cliente/lib/core/app_colors.dart
+++ gestion_cliente/lib/core/app_colors.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/animation.dart';
-
+//Colores para aplicar a las distintas ventanas de la App, se pueden agregar más o modificar los existentes
 class AppColors {
-  static const primaryColor = Color.fromARGB(255, 0, 98, 179)   
-  static const secondaryColor = Color.fromARGB(255, 3, 71, 218);
-  static const backgroundColor = Color.fromARGB(255, 28, 0, 150);  
-  static const errorColor = Color.fromARGB(171, 255, 30, 0);
+  static const primaryColor = Color.fromARGB(255, 21, 101, 192);   
+  static const secondaryColor = Color.fromARGB(255, 66, 165, 245);
+  static const backgroundColor = Color.fromARGB(255, 115, 183, 231);  
+
+  static const primaryColor2 = Color.fromARGB(255, 0, 151, 167);
+  static const secondaryColor2 = Color.fromARGB(255, 77, 208, 225);
+  static const backgroundColor2 = Color.fromARGB(255, 176, 240, 248);
+
+  static const primaryColor3 = Color.fromARGB(255, 0, 179, 45);
+  static const secondaryColor3 = Color.fromARGB(255, 10, 218, 3);
+  static const backgroundColor3 = Color.fromARGB(255, 0, 150, 112);
+
+  static const primaryColor4 = Color.fromARGB(255, 142, 36, 170);
+  static const secondaryColor4 = Color.fromARGB(255, 186, 104, 200);
+  static const backgroundColor4 = Color.fromARGB(255, 242, 194, 250);
+
+  static const primaryColor5 = Color.fromARGB(255, 198, 40, 40);
+  static const secondaryColor5 = Color.fromARGB(255, 239, 83, 80);
+  static const backgroundColor5 = Color.fromARGB(255, 243, 186, 194);
+
+  
 }
 
