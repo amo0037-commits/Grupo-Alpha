56861b225e32a89a730b88c2bcf80824ba1ad3dd
grupoalphamedac-2526 <grupoalphamedac2526@gmail.com>
2026-03-24 05:42:17 +0100

    Add files via upload

---
+++ RecogeDatos.dart
@@ -0,0 +1,14 @@
+
+import 'dart:io';
+void main(){
+ 
+  print('Ingresa tu nombre de usuario');
+  String? usuario = stdin.readLineSync();
+  print('Íngresa tu clave');
+String? clave=stdin.readLineSync();
+//print('Nombre de usuario ingresado: ${usuario}');
+//print('Clave ingresada: ${v_clave}');
+print(usuario);
+print(clave);
+
+}
