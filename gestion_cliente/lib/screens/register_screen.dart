e925a0c66a3ba21f7f2f4efa5c0c55ec4bbc1ed4
JuanEloySD <jsd0023@alu.medac.es>
2026-03-27 13:01:34 +0100

    Integración Firebase + Registro Básico

--- gestion_cliente/lib/screens/register_screen.dart
+++ gestion_cliente/lib/screens/register_screen.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class RegisterPage extends StatelessWidget {
+
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  Future<void> register() async {
+  if (passwordController.text != confirmPasswordController.text) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Las contraseñas no coinciden')),
+    );
+    return;
+  }
+
+  try {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Usuario creado correctamente')),
+    );
+
+    Navigator.pop(context);
+
+  } on FirebaseAuthException catch (e) {
+    String mensaje = 'Error';
+
+    if (e.code == 'email-already-in-use') {
+      mensaje = 'El email ya está en uso';
+    } else if (e.code == 'weak-password') {
+      mensaje = 'La contraseña es muy débil';
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(mensaje)),
+    );
+  }
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -25,15 +73,18 @@ class RegisterPage extends StatelessWidget {
             const SizedBox(height: 40),
 
             TextField(
+              controller: emailController,
               decoration: const InputDecoration(
                 labelText: 'Email',
-                border: OutlineInputBorder(),
+                   border: OutlineInputBorder(),
               ),
             ),
+
 
             const SizedBox(height: 20),
 
             TextField(
+              controller: passwordController,
               obscureText: true,
               decoration: const InputDecoration(
                 labelText: 'Contraseña',
@@ -43,22 +94,23 @@ class RegisterPage extends StatelessWidget {
 
             const SizedBox(height: 20),
 
-            TextField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Confirmar contraseña',
-                border: OutlineInputBorder(),
-              ),
+          TextField(
+            controller: confirmPasswordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: 'Confirmar contraseña',
+              border: OutlineInputBorder(),
             ),
+          ),
+
+
 
             const SizedBox(height: 30),
 
-            ElevatedButton(
-              onPressed: () {
-                // lógica registro
-              },
-              child: const Text('Registrarse'),
-            ),
+          ElevatedButton(
+            onPressed: register,
+            child: const Text('Registrarse'),
+          ),
           ],
         ),
       ),
