6f6634a625b7b4990c09bf298f0e82c13b3f27f3
ils0007 <ils0007@alu.medac.es>
2026-04-07 11:49:18 +0200

    terminado base de la pagina de descripcion de servicios

--- gestion_cliente/lib/screens/services_screen.dart
+++ gestion_cliente/lib/screens/services_screen.dart
@@ -35,59 +35,210 @@ class ServicePage extends StatelessWidget {
           ),
 
         body: SingleChildScrollView(
-          child: Center(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
 
                 children: [
-                  const SizedBox(height: 60),
+                  const SizedBox(height:60),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                  Image.asset(
-                    'assets/images/peluqueriainfo.png',
-                    width: MediaQuery.of(context).size.width * 0.2,
-                    fit: BoxFit.contain,
-                  ),
-                ],
-              ),
-                  const SizedBox(width: 15),
-
-                  const Text(
-                    'Peluqueria',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 60),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                  Image.asset(
-                    'assets/images/clinicafisioinfo.png',
-                    width: MediaQuery.of(context).size.width * 0.2,
-                    fit: BoxFit.contain,
-                  ),
-                ],
-              ),
-                  const SizedBox(width: 15),
-
-                  const Text(
-                    'Clinica de fisioterapia',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 60),
-                ]
-                                  
-             ),
-            ),
+                 Row(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Column(
+      children: [
+        Image.asset(
+          'assets/images/peluqueriainfo2.png',
+          width: MediaQuery.of(context).size.width * 0.2,
+          fit: BoxFit.contain,
+        ),
+        const SizedBox(height: 8),
+        const Text(
+          'Peluqueria',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
           ),
         ),
-       ),
-      );    
-    
-   
+      ],
+    ),
 
+    const SizedBox(width: 20),
+
+    const Expanded(
+      child: Text(
+        'Ofrecemos un servicio de peluqueria y estetica',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  ],
+),
+                
+
+                  
+                  const SizedBox(height: 60),
+                  Row(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Column(
+      children: [
+        Image.asset(
+          'assets/images/clinicafisioinfo.png',
+          width: MediaQuery.of(context).size.width * 0.2,
+          fit: BoxFit.contain,
+        ),
+        const SizedBox(height: 8),
+        const Text(
+          'Clinica de fisioterapia',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+
+    const SizedBox(width: 20),
+
+    const Expanded(
+      child: Text(
+        'Ofrecemos un servicio de fisioterapia para rehabilitación y recuperación',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  ],
+),
+                  const SizedBox(height: 60),
+
+
+                 Row(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Column(
+      children: [
+        Image.asset(
+          'assets/images/gimnasioinfo.png',
+          width: MediaQuery.of(context).size.width * 0.2,
+          fit: BoxFit.contain,
+        ),
+        const SizedBox(height: 8),
+        const Text(
+          'Gimnasio',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+
+    const SizedBox(width: 20),
+
+    const Expanded(
+      child: Text(
+        'Ofrecemos un servicio de gimnasio altamente preparado y equipado',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  ],
+),
+
+                   const SizedBox(height: 60),
+
+                  Row(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Column(
+      children: [
+        Image.asset(
+          'assets/images/yogainfo.png',
+          width: MediaQuery.of(context).size.width * 0.2,
+          fit: BoxFit.contain,
+        ),
+        const SizedBox(height: 8),
+        const Text(
+          'Centro de yoga y pilates',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+
+    const SizedBox(width: 20),
+
+    const Expanded(
+      child: Text(
+        'Ofrecemos un servicio de yoga y pilates perfecto para la relajación y paz mental',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  ],
+),
+                  
+                   const SizedBox(height: 60),
+
+
+                 Row(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Column(
+      children: [
+        Image.asset(
+          'assets/images/academiainfo.png',
+          width: MediaQuery.of(context).size.width * 0.2,
+          fit: BoxFit.contain,
+        ),
+        const SizedBox(height: 8),
+        const Text(
+          'Academia',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+
+    const SizedBox(width: 20),
+
+    const Expanded(
+      child: Text(
+        'Ofrecemos un servicio de academias de muchas asignaturas y especialidades',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  ],
+),
+
+                  
+                    ],
+                  ),
+                  ),              
+             ),
+            ),
+          );
   }
 }
